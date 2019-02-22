fc6cfc3c96354f9163fdcd48954cc95cd1699698
Nicholas Ionata <n.ionata129@gmail.com>
2019-02-22 15:19:40 -0500

    Added wireframe client

---
+++ client/js/controllers/controller.js
@@ -0,0 +1,5 @@
+//Replace moduleName //Replace controller //Import factoryName
+angular.module('moduleName').controller('controller', ['$scope',
+  function($scope) {
+  }
+]);
---
+++ client/js/factories/factory.js
@@ -0,0 +1,6 @@
+angular.module('moduleName', []).factory('factoryName', function($http) {
+  var methods = {
+  };
+
+  return methods;
+});
