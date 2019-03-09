04a64a7a43812ca9fbe417c8552631d137f7c485
Jake Meisenheimer <meisenheimerj@ufl.edu>
2019-03-09 16:32:02 -0500

    change factory.js to use relative path

--- client/js/factories/factory.js
+++ client/js/factories/factory.js
@@ -1,7 +1,7 @@
 angular.module('moduleName', []).factory('factoryName', function($http, $window) {
   var methods = {
       create: function(user) {
-	  return $http.post('http://localhost:8080/reg', user).success(function(data, status, headers, config) {
+	  return $http.post('/reg', user).success(function(data, status, headers, config) {
           // if the response is data is success then redirect to the login otherwise send a message
             if(data == "success"){
                  $window.location=('login');   
@@ -13,7 +13,7 @@ angular.module('moduleName', []).factory('factoryName', function($http, $window)
     },
       
       login: function(user) {
-	  return $http.post('http://localhost:8080/login', user).success(function(data, status, headers, config) {
+	  return $http.post('/login', user).success(function(data, status, headers, config) {
           // if the response is data is success then redirect to the dashboard otherwise send a message
             if(data == "success"){
                  $window.location=('dashboard');   
