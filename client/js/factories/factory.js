b04ff5550991f531bfa44729b41831fdbb03529b
Nicholas Ionata <n.ionata129@gmail.com>
2019-03-19 16:17:02 -0400

    Updated how client handles api responses

--- client/js/factories/factory.js
+++ client/js/factories/factory.js
@@ -1,27 +1,24 @@
 angular.module('moduleName', []).factory('factoryName', function($http, $window) {
   var methods = {
-      create: function(user) {
-	  return $http.post('/api/user/register', user).success(function(data, status, headers, config) {
+    create: function(user) {
+      return $http.post('/api/user/register', user).success(function(data, status, headers, config) {
           // if the response is data is success then redirect to the login otherwise send a message
-            if(data == "success"){
-                 $window.location=('login');
-            }else
-            {
-                alert(data);
-            }
-  })
+          if(data.message === "User created successfully") {
+            $window.location=('login');
+          } else {
+            alert(data.message);
+          }
+      })
     },
-
-      login: function(user) {
-	  return $http.post('/api/user/login', user).success(function(data, status, headers, config) {
-          // if the response is data is success then redirect to the dashboard otherwise send a message
-            if(data == "success"){
-                 $window.location=('dashboard');
-            }else
-            {
-                alert(data);
-            }
-  })
+    login: function(user) {
+      return $http.post('/api/user/login', user).success(function(data, status, headers, config) {
+        // if the response is data is success then redirect to the dashboard otherwise send a message
+        if(data.message === "User signed in successfully") {
+          $window.location=('dashboard');
+        } else {
+          alert(data.message);
+        }
+      })
     }
   };
 
