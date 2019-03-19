13a0d78421dee9459247e8c2e120b63538a5a3c9
Nicholas Ionata <n.ionata129@gmail.com>
2019-03-19 01:16:59 -0400

    Updated new api endpoint on client

--- client/js/factories/factory.js
+++ client/js/factories/factory.js
@@ -1,22 +1,22 @@
 angular.module('moduleName', []).factory('factoryName', function($http, $window) {
   var methods = {
       create: function(user) {
-	  return $http.post('/reg', user).success(function(data, status, headers, config) {
+	  return $http.post('/api/user/register', user).success(function(data, status, headers, config) {
           // if the response is data is success then redirect to the login otherwise send a message
             if(data == "success"){
-                 $window.location=('login');   
+                 $window.location=('login');
             }else
             {
                 alert(data);
             }
   })
     },
-      
+
       login: function(user) {
-	  return $http.post('/login', user).success(function(data, status, headers, config) {
+	  return $http.post('/api/user/login', user).success(function(data, status, headers, config) {
           // if the response is data is success then redirect to the dashboard otherwise send a message
             if(data == "success"){
-                 $window.location=('dashboard');   
+                 $window.location=('dashboard');
             }else
             {
                 alert(data);
