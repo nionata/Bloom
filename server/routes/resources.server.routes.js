8392a77d5b089c1f6c65403ae026177c3eff97b4
Nicholas Ionata <n.ionata129@gmail.com>
2019-04-22 20:21:47 -0400

    Implemented the resources routes and controller

--- server/routes/resources.server.routes.js
+++ server/routes/resources.server.routes.js
@@ -1,9 +1,12 @@
 /* Dependencies */
-var user = require('../controllers/resources.server.controller.js'),
+var resources = require('../controllers/resources.server.controller.js'),
     express = require('express'),
     router = express.Router();
 
 //These method calls are responsible for routing requests to the correct request handler.
-router.route('')
+router.route('').get(resources.getAll);
+router.route('/:id').get(resources.getById)
+                   .delete(resources.delete);
+router.route('/create').post(resources.create);
 
 module.exports = router;
